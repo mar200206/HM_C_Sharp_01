725b179d664db6e4eda1f55e09726ea9904b2e14
sasha <you@example.com>
2023-03-18 14:37:46 +0300

    Tack 02

---
+++ homework_1/1_0/1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿int max = 0;
+
+Console.Write("Enter the a number: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the b number: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the c number: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a  > max)
+{
+      max = a ;
+}
+if (b  > max)
+{
+      max = b ;
+}
+if (c  > max)
+{
+      max = c;
+}
+
+Console.WriteLine("max = " + "max");
+
--- homework_1/1_0/Program.cs
+++ homework_1/1_0/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+   max = a;
+   min = b;
+}
+Console.WriteLine("max" + "max" + " and " + "min" + "min");
+
+
